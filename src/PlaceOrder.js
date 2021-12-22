6de5bc74f3bee6822cde2d13e25a7c584f95987e
Subhamdas20 <Subham21.sd@gmail.com>
2021-12-22 16:31:16 +0530

    curring function

---
+++ src/PlaceOrder.js
@@ -0,0 +1,10 @@
+const PlaceOrder = (email)=>{
+    return (earphone)=>{
+        return (price)=>{
+            console.log(price)
+        }
+    }
+}
+
+let order = PlaceOrder("subham20.sd@gmail.com")("JBL")
+order(256)
